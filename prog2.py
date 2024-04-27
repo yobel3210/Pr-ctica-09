850eec8c9f3a1705e4f945d9092fbc5c31126bf4
yobel3210 <doloreszeferinoyobel@gmail.com>
2024-04-27 13:18:22 -0600

    Versión que encuentra la palabra más larga y la palabra más corta de una oración

--- prog2.py
+++ prog2.py
@@ -19,13 +19,23 @@ def find_longest_word(line):
 			longest = word
 	return longest
 
+def find_shortest_word(line):
+	words = line.split(" ")
+	shortest = words[0]
+	for word in words:
+		if len(word) < len(shortest):
+			shortest = word
+	return shortest
+
 def main(argv):
 	if len(argv) < 2:
 		return
 	line = " ".join(argv[1:])
 	print("Line is:", line)
 	longest = find_longest_word(line)
+	shortest = find_shortest_word(line)
 	print("Longest is:", longest)
+	print("Shortest is:", shortest)
 
 if __name__ == '__main__':
 	main(sys.argv)
