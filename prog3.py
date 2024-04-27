1d6eff14fb8cacfeac0316286b3a5030bec84e54
yobel3210 <doloreszeferinoyobel@gmail.com>
2024-04-27 15:41:01 -0600

    Cuenta la cantidad de ocurrencias de cada palabra encontrada e imprime el top 10 de las palabras más usadas en ese texto, no toma en cuenta el numero de letras

---
+++ prog3.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# ## ###############################################################
+# prog1.py
+#
+# Author: Mauricio Matamoros
+# License: MIT
+#
+# Finds the word that occurs the most times in a given text
+#
+# ## ###############################################################
+import os
+import re
+
+def rank_words(wc):
+	ranked_words = []
+	for word in wc:
+		ranked_words.append( (word, wc[word]) )
+	ranked_words.sort(key=lambda tup : tup[1], reverse=True)
+	return ranked_words
+
+
+
+def count_words(filepath):
+	wordcount = {}
+	with open(filepath, "r") as f:
+		line = f.readline()
+		while line:
+			words = re.split(r"\W+", line)
+			for word in words:
+				if not word:
+					continue
+				word = word.lower()
+				if word in wordcount:
+					wordcount[word] += 1
+				else:
+					wordcount[word] = 1
+			line = f.readline()
+	return wordcount
+
+
+
+def main():
+	wordcount = count_words("quijote.txt")
+	ranked = rank_words(wordcount)
+	for i in range(10):
+		print('Most used word #{}: "{}" ({})'
+			.format(
+				i+1,
+				ranked[i][0],
+				ranked[i][1]
+			)
+		)
+
+
+
+if __name__ == '__main__':
+	main()
